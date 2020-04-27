249d1aaddf13f1b072a8c91dc6596c131b003429
Krista Rutz <rutzkri000@gmail.com>
2020-04-27 14:33:47 -0700

    make a store and subscribe, adding and deleting  sample objects
    Co-authored-by: Steven Fleming <12flemings@gmail.com>

--- learning-about-redux.js
+++ learning-about-redux.js
@@ -18,3 +18,34 @@ const ticketListReducer = (state = {}, action) => {
       return state;
   }
 };
+
+const { createStore } = Redux;
+
+const store = createStore(ticketListReducer);
+
+console.log(store.getState());
+
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
+store.dispatch({
+  type: "ADD_TICKET",
+  names: "Jasmine and Justine",
+  location: "3b",
+  issue: "Problems understanding Redux.",
+  id: 1,
+});
+
+store.dispatch({
+  type: "ADD_TICKET",
+  names: "Brann and Rose",
+  location: "3b",
+  issue: "Problems understanding Redux.",
+  id: 2,
+});
+
+store.dispatch({
+  type: "DELETE_TICKET",
+  id: 1,
+});
+
+unsubscribe();
